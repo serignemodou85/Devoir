6700a762f93e2640cbe99fdf40fcb87081b1e899
modou <tellofall@gmail.com>
2023-12-26 16:59:30 +0000

    valider

---
+++ Exercice1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border=1>
+        <tr>
+            <td>Valide</td>
+            <td>Non valide</td>
+        </tr>
+        <tr>
+            <td>$mavar <br>$_mavar <br>$_5var <br>$_mavar<br>$element1 <br>mavar </td>
+            <td>$hote14*</td>
+        </tr>
+    </table>
+</body>
+</html>
