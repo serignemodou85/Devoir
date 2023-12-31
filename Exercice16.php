e6960c5a9796e1b8ec849c71a463520bb9eb53a3
modou <tellofall@gmail.com>
2023-12-31 19:57:07 +0000

    valider

--- Exercice16.php
+++ Exercice16.php
@@ -1,5 +1,9 @@
 <?php
 function sinus($angle, $unit = 'radian') {
+    if (!is_numeric($angle)) {
+        return 'Erreur: L\'angle doit être un nombre.';
+    }
+
     switch ($unit) {
         case 'degre': // degrés
             $angle = deg2rad($angle);
@@ -14,8 +18,7 @@ function sinus($angle, $unit = 'radian') {
     return sin($angle);
 }
 
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -25,5 +28,27 @@ function sinus($angle, $unit = 'radian') {
 </head>
 <body>
     <h1>Exercice 16: Fonction de calcul du sinus</h1>
+
+    <form action="" method="get">
+        <label for="angle">Angle:</label>
+        <input type="text" id="angle" name="angle" required>
+        <br>
+        <label for="unit">Unité:</label>
+        <select id="unit" name="unit">
+            <option value="radian">Radians</option>
+            <option value="degre">Degrés</option>
+            <option value="grade">Grades</option>
+        </select>
+        <br>
+        <input type="submit" value="Calculer le sinus">
+    </form>
+
+    <?php
+    if (isset($_GET['angle'])) {
+        $angle = $_GET['angle'];
+        $unit = $_GET['unit'];
+        echo "<p>Résultat: " . sinus($angle, $unit) . "</p>";
+    }
+    ?>
 </body>
 </html>
